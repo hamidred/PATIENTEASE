2bd4423c85508355890ce0e98d4ba94185da14fa
Abdelhamid Tebbal <tebbal.a.elhamid@gmail.com>
2025-01-30 00:12:21 +0100

    frontend part ready

--- frontend/src/assets/data/services.js
+++ frontend/src/assets/data/services.js
@@ -1,38 +1,50 @@
+import service01 from "../images/service01.png";
+import service02 from "../images/service02.png";
+import service03 from "../images/service03.png";
+import service04 from "../images/service04.png";
+import service05 from "../images/service05.png";
+import service06 from "../images/service06.png";
 export const services = [
   {
-    name: "Cancer Care",
+    name: "General",
     desc: "World-class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
     bgColor: "rgba(254, 182, 13, .2)",
     textColor: "#FEB60D",
+    img: service01,
   },
   {
-    name: "Labor & Delivery",
+    name: "Pediatrics",
     desc: "World-class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
     bgColor: "rgba(151, 113, 255, .2)",
     textColor: "#9771FF",
+    img: service02,
   },
   {
-    name: "Heart & Vascular",
+    name: "Nutritional",
     desc: "World-class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
     bgColor: "rgba(1, 181, 197, .2)",
     textColor: "#01B5C5",
+    img: service03,
   },
   {
-    name: "Mental Health",
+    name: "Cardiology",
     desc: "World-class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
     bgColor: "rgba(1, 181, 197, .2)",
     textColor: "#01B5C5",
+    img: service04,
   },
   {
-    name: "Neurology",
+    name: "Ophthalmology",
     desc: "World-class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
     bgColor: "rgba(254, 182, 13, .2)",
     textColor: "#FEB60D",
+    img: service05,
   },
   {
-    name: "Burn Treatment",
+    name: "Orthopedics",
     desc: "World-class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
     bgColor: "rgba(151, 113, 255, .2)",
     textColor: "#9771FF",
+    img: service06,
   },
 ];
--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,25 +1,24 @@
-import React from 'react'
+import React from "react";
 
-import { Link } from 'react-router-dom'
-import logo from '../../assets/images/logo.png'
-import {RiLinkedinFill} from 'react-icons/ri'
-import { AiFillGithub, AiOutlineInstagram} from 'react-icons/ai'
-
+import { Link } from "react-router-dom";
+import logo from "../../assets/images/logo.png";
+import { RiLinkedinFill } from "react-icons/ri";
+import { AiFillGithub, AiOutlineInstagram } from "react-icons/ai";
 
 const socialLinks = [
   {
-    path: "https://www.linkedin.com/in/mohamedboukthir/",
-    icon: <RiLinkedinFill className='group-hover:text-white w-4 h-5'/>,
+    path: "https://www.linkedin.com/in/abdelhamid-tebbal/",
+    icon: <RiLinkedinFill className="group-hover:text-white w-4 h-5" />,
   },
 
   {
-    path: "https://github.com/MohamedBoukthir",
-    icon: <AiFillGithub className='group-hover:text-white w-4 h-5'/>,
+    path: "https://github.com/hamidred/",
+    icon: <AiFillGithub className="group-hover:text-white w-4 h-5" />,
   },
 
   {
-    path: "https://www.instagram.com/hamaboukthir.exe/",
-    icon: <AiOutlineInstagram className='group-hover:text-white w-4 h-5'/>,
+    path: "https://www.instagram.com/abelhamid_tebbal/",
+    icon: <AiOutlineInstagram className="group-hover:text-white w-4 h-5" />,
   },
 ];
 
@@ -79,92 +78,89 @@ const quickLinks03 = [
   },
 ];
 
-
 const Footer = () => {
-
-  const year = new Date().getFullYear()
+  const year = new Date().getFullYear();
 
   return (
-    <footer className='pb-16 pt-10'>
+    <footer className="pb-16 pt-10">
       <div className="container">
-        <div className='flex justify-between flex-col md:flex-row flew-wrap gap-[30px] '>
+        <div className="flex justify-between flex-col md:flex-row flew-wrap gap-[30px] ">
           <div>
             <img src={logo} alt="" />
 
-            <p className='text-[16px] leading-7 font-[400] text-textColor mt-4'>
+            <p className="text-[16px] leading-7 font-[400] text-textColor mt-4">
               Copyright © {year} <br />
-               Developed By  <b>Mohamed Boukthir</b> <br />
-                All Right Reserved.
+              Developed By <b>Abdelhamid Tebbal</b> <br />
+              All Right Reserved.
             </p>
 
-            <div className='flex items-center gap-3 mt-4'>
+            <div className="flex items-center gap-3 mt-4">
               {socialLinks.map((link, index) => (
-                <Link 
-                 to={link.path}
-                 key={index}
-                 className='w-9 h-9 border border-solid border-[#181A1E] rounded-full flex items-center justify-center group hover:bg-primaryColor hover:border-none'
-                 >
+                <Link
+                  to={link.path}
+                  key={index}
+                  className="w-9 h-9 border border-solid border-[#181A1E] rounded-full flex items-center justify-center group hover:bg-primaryColor hover:border-none"
+                >
                   {link.icon}
                 </Link>
               ))}
             </div>
-
           </div>
           <div>
-            <h2 className='text-[20px] leading-[30px] font-[700] mb-6 text-headingColor'>
+            <h2 className="text-[20px] leading-[30px] font-[700] mb-6 text-headingColor">
               Quick Links
             </h2>
             <ul>
-              {quickLinks01.map((item , index ) => ( 
-               <li key={index} className='mb-4'>
-                <Link
-                  to={item.path}
-                className='text-[16px] leading-7 font-[400] text-textColor'
-                > 
-                  {item.display}
-                </Link>
+              {quickLinks01.map((item, index) => (
+                <li key={index} className="mb-4">
+                  <Link
+                    to={item.path}
+                    className="text-[16px] leading-7 font-[400] text-textColor"
+                  >
+                    {item.display}
+                  </Link>
                 </li>
-                ))}
+              ))}
             </ul>
           </div>
           <div>
-            <h2 className='text-[20px] leading-[30px] font-[700] mb-6 text-headingColor'>
-              I Want To 
+            <h2 className="text-[20px] leading-[30px] font-[700] mb-6 text-headingColor">
+              I Want To
             </h2>
             <ul>
-              {quickLinks02.map((item , index ) => ( 
-               <li key={index} className='mb-4'>
-                <Link
-                  to={item.path}
-                className='text-[16px] leading-7 font-[400] text-textColor'
-                > 
-                  {item.display}
-                </Link>
+              {quickLinks02.map((item, index) => (
+                <li key={index} className="mb-4">
+                  <Link
+                    to={item.path}
+                    className="text-[16px] leading-7 font-[400] text-textColor"
+                  >
+                    {item.display}
+                  </Link>
                 </li>
-                ))}
+              ))}
             </ul>
           </div>
           <div>
-            <h2 className='text-[20px] leading-[30px] font-[700] mb-6 text-headingColor'>
+            <h2 className="text-[20px] leading-[30px] font-[700] mb-6 text-headingColor">
               Support
             </h2>
             <ul>
-              {quickLinks03.map((item , index ) => ( 
-               <li key={index} className='mb-4'>
-                <Link
-                  to={item.path}
-                className='text-[16px] leading-7 font-[400] text-textColor'
-                > 
-                  {item.display}
-                </Link>
+              {quickLinks03.map((item, index) => (
+                <li key={index} className="mb-4">
+                  <Link
+                    to={item.path}
+                    className="text-[16px] leading-7 font-[400] text-textColor"
+                  >
+                    {item.display}
+                  </Link>
                 </li>
-                ))}
+              ))}
             </ul>
           </div>
         </div>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
